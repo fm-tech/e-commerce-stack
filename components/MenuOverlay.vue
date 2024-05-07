fdd8369e4f7e6c8c1ee75e8cf2b453606ff3af49
Francisco <fdmiran@gmail.com>
2024-05-07 16:59:24 -0500

    Updated authentication and overlay.

---
+++ components/MenuOverlay.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="MenuOverlay" class="fixed z-50 bottom-0 h-full w-full
+    bg-white px-3
+    ">
+        <div class="flex items-center justify-between py-5">
+            <NuxtLink to="/" @click="userStore.isMenuOverlay = false">
+                <img 
+                    width="170"
+                    src="/AliExpress-logo.png" 
+                    alt="">
+            </NuxtLink>
+
+            <button
+                @click="userStore.isMenuOverlay = false"
+                class="rounded-full p-1 hover:bg-gray-200"
+            >
+                <Icon name="mdi:close" size="30" />
+            </button>
+        </div>
+        <div 
+        class="flex items-center justify-between pt-5">
+            <ul class="w-full">
+                <li
+                    @click="goTo('orders')"
+                    class="relative"
+                >
+
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useUserStore } from '~/stores/user'
+const userStore = useUserStore()
+
+// const client    = useSupabseClient()
+// const user      = useSupabaseUser()      
+
+const goTo = (url) => {
+    userStore.isMenuOverlay = false
+    return navigateTo(`/${url}`)
+}
+
+const signOut = () => {
+    client.auth.signOut()
+    userStore.isMenuOverlay = false
+    return navigateTo('/')
+}
+
+const signIn = () => {
+    userStore.isMenuOverlay = false
+    return navigateTo('/auth')
+}
+
+</script>
---
+++ pages/auth.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="AuthPage" class="w-full h-[100vh] bg-white">
+        <!-- Your authentication form goes here -->
+        <div class="w-full flex items-center justify-center p-5 border-b border-b-gray-300">
+            <NuxtLink to="/" class="min-w-[170px]">
+                <img width="170" src="/AliExpress-logo.png" alt="Logo">
+            </NuxtLink>
+        </div>
+
+        <div class="max-w-[400px] mx-auto px-2">
+            <div class="text-center my-6">Login / Register</div>
+            <button @click="login(google)" class="flex
+                    items-center
+                    justify-center
+                    gap-3
+                    p-1.5
+                    w-full
+                    border
+                    hover:bg-gray-100
+                    rounded-full
+                    text-lg
+                    font-semibold
+                    ">
+                <img class="w-full max-w-[30px]" src="/google-logo.png" alt="google">
+                <div>Google</div>
+            </button>
+            <button @click="login(github)" class="
+                    mt-4
+                    flex
+                    items-center
+                    justify-center
+                    gap-3
+                    p-1.5
+                    w-full
+                    border
+                    hover:bg-gray-100
+                    rounded-full
+                    text-lg
+                    font-semibold
+                    ">
+                <img class="w-full max-w-[30px]" src="/github-logo.png" alt="google">
+                <div>Google</div>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+// const client    = useSupabseClient()
+// const user      = useSupabaseUser()
+
+// watchEffect(() => {
+//     if(user.value) {
+//         return navigateTo('/')
+//     }
+// })
+
+const login = async (prov) => {
+    const {data, error} = await client.auth.signInWithOAuth({
+        provider: prov,
+    })
+}
+
+</script>
+
