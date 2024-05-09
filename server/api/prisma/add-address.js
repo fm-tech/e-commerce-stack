965dddfd3ed5d4f1d90f6dc794c4b6816159cb68
Francisco <fdmiran@gmail.com>
2024-05-09 12:07:17 -0500

    initialized prisma migration

---
+++ server/api/prisma/add-address.js
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async, (event) => {
+    const body = await readBody(event)
+
+    const order = await prisma.addresses.create({
+        data: {
+            userId: body.userId,
+            name: body.name,
+            address: body.address
+        }
+    })
+})
